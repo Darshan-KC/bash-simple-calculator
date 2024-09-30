1c67b0847adf3e2696ce422ece3c28e00c6ec7d8
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-09-30 21:04:29 +0545

    calculator complete

--- calculator.sh
+++ calculator.sh
@@ -7,4 +7,40 @@ read num1
 
 read -p "Enter second number: " num2
 
-read -p "Enter operator +,-,*,/,% :" op
+read -p "Enter operator +,-,*,/,% : " op
+
+case $op in 
+    +)
+     result=$((num1 + num2))
+     ;;
+
+    -)
+     result=$((num1 - num2))
+     ;;
+
+    \*)
+     result=$((num1 * num2))
+     ;;
+
+    /)
+    if [ $num2 -ne 0 ]; then
+        result=$((num1 / num2))
+    else
+      echo "Division by zero is not allowed."
+      exit 1
+    fi
+     ;;
+
+    %)
+     result=$((num1 % num2))
+     ;;
+
+    *)
+     echo "Invalid operator"
+     exit 1
+     ;;
+esac
+
+# print the result
+echo "The result is : $result"
+     
